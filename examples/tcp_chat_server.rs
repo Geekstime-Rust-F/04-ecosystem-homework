2f1d077a368ba265929b0787417291b44ba6aac6
Fuhua Zhang <3102207728@qq.com>
2024-07-09 01:02:28 +0800

    feat: add user join and left message and refactor

--- examples/tcp_chat_server.rs
+++ examples/tcp_chat_server.rs
@@ -1,4 +1,9 @@
-use std::{collections::HashMap, net::SocketAddr, sync::Arc};
+use std::{
+    collections::HashMap,
+    fmt::{self, Display},
+    net::SocketAddr,
+    sync::Arc,
+};
 
 use anyhow::Result;
 use futures::{SinkExt, StreamExt};
@@ -14,7 +19,21 @@ const MAX_CHANNEL_SIZE: usize = 32;
 
 #[derive(Debug)]
 struct ChatState {
-    users: HashMap<SocketAddr, mpsc::Sender<String>>,
+    users: HashMap<SocketAddr, User>,
+}
+
+#[derive(Debug)]
+enum Message {
+    UserJoined(String),
+    UserLeft(String),
+    Chat { from: String, message: String },
+}
+
+#[derive(Debug, Clone)]
+#[allow(unused)]
+struct User {
+    name: String,
+    sender: mpsc::Sender<String>,
 }
 
 #[tokio::main]
@@ -51,10 +70,21 @@ async fn handle_connection(
     addr: SocketAddr,
     state: Arc<Mutex<ChatState>>,
 ) -> Result<()> {
-    let stream = Framed::new(stream, LinesCodec::new());
+    let mut stream = Framed::new(stream, LinesCodec::new());
     info!("Handling connection: {}", addr);
+
+    let mut state = state.lock().await;
+
     let (tx, mut rx) = mpsc::channel(MAX_CHANNEL_SIZE);
-    state.lock().await.add_user(addr, tx);
+    stream.send("Please enter your name: ".to_string()).await?;
+    let name = match stream.next().await {
+        Some(Ok(name)) => name,
+        _ => anyhow::Error::msg("Failed to get name from user").to_string(),
+    };
+    state.add_user(addr, tx, &name);
+    state
+        .broadcast(Message::UserJoined(name.clone()), addr)
+        .await?;
 
     let (mut writer, mut reader) = stream.split();
 
@@ -75,21 +105,52 @@ async fn handle_connection(
         };
         info!("Received message: {}", line);
 
-        for (user_addr, tx) in state.lock().await.users.iter() {
-            if *user_addr != addr {
-                info!("Sending message to channel: {}", user_addr);
-                tx.send(line.clone()).await?;
-            }
-        }
+        state
+            .broadcast(
+                Message::Chat {
+                    from: name.clone(),
+                    message: line,
+                },
+                addr,
+            )
+            .await?;
     }
+    state.users.remove(&addr);
+    state
+        .broadcast(Message::UserLeft(name.clone()), addr)
+        .await?;
     info!("Connection closed: {}", addr);
 
     Ok(())
 }
 
 impl ChatState {
-    fn add_user(&mut self, addr: SocketAddr, sender: mpsc::Sender<String>) {
-        self.users.insert(addr, sender);
+    fn add_user(&mut self, addr: SocketAddr, sender: mpsc::Sender<String>, name: &str) {
+        let user = User {
+            name: name.to_string(),
+            sender,
+        };
+        self.users.insert(addr, user.clone());
         info!("current users: {:?}", self.users.keys());
+    }
+
+    async fn broadcast(&mut self, msg: Message, addr: SocketAddr) -> Result<()> {
+        for (user_addr, tx) in self.users.iter() {
+            if *user_addr != addr {
+                info!("Sending message to channel: {}", user_addr);
+                tx.sender.send(msg.to_string()).await?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Message::UserJoined(name) => write!(f, "{} joined the chat", name),
+            Message::UserLeft(name) => write!(f, "{} left the chat", name),
+            Message::Chat { from, message } => write!(f, "{}: {}", from, message),
+        }
     }
 }
